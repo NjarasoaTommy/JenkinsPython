ef4e18d241d4ec3dacff04c6867398cfed4d8b1d
Njarasoa Tommy <njarasoatommy@gmail.com>
2025-08-02 17:08:37 +0300

    Create a simple python script

---
+++ script.py
@@ -0,0 +1,12 @@
+def fibo(n):
+    if n == 0 or n == 1:
+        return 1
+    elif n < 0:
+        raise ValueError("The parameter should be a positive number")
+    else:
+        return fibo(n - 1) + fibo(n - 2)
+    
+print(fibo(0))
+print(fibo(1))
+print(fibo(10))
+print(fibo(-1))
